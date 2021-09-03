9412cc293a84eaa100bbd158e6fb3b0b37af0181
mikhail chichaykin <mikhail.chichaykin@grabtaxi.com>
2021-09-03 19:22:50 +0300

    fix TickerServiceImpl

--- src/leetcode/google/TickerService.kt
+++ src/leetcode/google/TickerService.kt
@@ -15,42 +15,31 @@ internal interface TickerService {
 }
 
 internal class TickerServiceImpl : TickerService {
-    private val history = TreeMap<Long, Double>()
-    private var curMinTime: Long = 0
+    private val history = HashMap<Long, Double>()
+    private val minHeap = PriorityQueue<Double>()
+    private var lastValueTime = Long.MIN_VALUE
 
-    override fun correctPrice(price: Double, timestamp: Long) {
-        val oldPrice = history[timestamp]
-        if (oldPrice != null && curMinTime == timestamp && oldPrice < price) {
-            // Trace all right values to find real min
-            for ((key, value) in history.tailMap(curMinTime, false)) {
-                if (value < price) {
-                    curMinTime = key
-                    break
-                }
-            }
+    override fun correctPrice(newPrice: Double, timestamp: Long) {
+        history[timestamp]?.let { oldPrice ->
+            minHeap.remove(oldPrice) //O(log N)
         }
-        history[timestamp] = price
-        if (history[curMinTime]!! > price) {
-            curMinTime = timestamp
-        }
+        history[timestamp] = newPrice
+        minHeap.add(newPrice) //O(log N)
     }
 
     override fun setCurrentPrice(price: Double, timestamp: Long) {
-        if (history.isEmpty()) curMinTime = timestamp
         history[timestamp] = price
-        history[curMinTime]?.let {
-            if (it > price) curMinTime = timestamp
-        }
+        minHeap.add(price)
+        lastValueTime = timestamp
     }
 
     override val lastValue: Double
-        get() = if (history.isEmpty()) 0.0 else history.lastEntry().value
+        get() {
+            return history[lastValueTime] ?: 0.0
+        }
     override val min: Double
         get() {
-            history[curMinTime]?.let {
-                return it
-            }
-            return 0.0
+            return if (minHeap.isNotEmpty()) minHeap.first() else 0.0
         }
 }
 
@@ -60,14 +49,18 @@ class TickerServiceImplTest {
         val service = TickerServiceImpl()
         service.setCurrentPrice(9.0, 1)
         assertEquals(9.0, service.min)
+
         service.setCurrentPrice(8.0, 2)
         assertEquals(8.0, service.min)
-        service.setCurrentPrice(8.0, 3)
+
+        service.setCurrentPrice(7.0, 3)
+        assertEquals(7.0, service.min)
+
+        service.correctPrice(10.0, 3)
         assertEquals(8.0, service.min)
-        service.correctPrice(10.0, 2)
-        assertEquals(8.0, service.min)
+
         service.correctPrice(7.0, 2)
         assertEquals(7.0, service.min)
-        assertEquals(8.0, service.lastValue)
+        assertEquals(10.0, service.lastValue)
     }
 }
